11e146bb4b0cb787ca8ca7d961857b16007d9a0d
bosiarquitetura <bosiarquitetura@gmail.com>
2019-12-01 14:45:10 -0300

    Finished version 1

---
+++ bot.py
@@ -0,0 +1,22 @@
+import tweepy
+from os import environ
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+searches = api.search(q='tea', lang='en', count=25, result_type='recent')
+
+best_tweet = 0
+
+for i in range(len(searches)):
+    if searches[i].retweet_count > searches[best_tweet].retweet_count:
+        best_tweet = i
+
+id = searches[best_tweet].id
+
+api.retweet(id)
