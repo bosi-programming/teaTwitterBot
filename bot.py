b81de9ada21067965256f266023edade061c7799
bosiarquitetura <bosiarquitetura@gmail.com>
2019-12-02 12:05:54 -0300

    Correcting some semantic problems

--- bot.py
+++ bot.py
@@ -9,14 +9,17 @@ auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-searches = api.search(q='tea', lang='en', count=25, result_type='recent')
+def searches(): 
+    api.search(q='tea', lang='en', count=25, result_type='recent')
 
-best_tweet = 0
+    best_tweet = 0
 
-for i in range(len(searches)):
-    if searches[i].retweet_count > searches[best_tweet].retweet_count:
-        best_tweet = i
+    for i in range(len(searches)):
+        if searches[i].retweet_count > searches[best_tweet].retweet_count:
+            best_tweet = i
 
-id = searches[best_tweet].id
+    id = searches[best_tweet].id
 
-api.retweet(id)
+    api.retweet(id)
+
+searches()
